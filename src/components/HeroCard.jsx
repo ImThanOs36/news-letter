8a6081b88c1301087802664ff828b8f7bef4a5af
ImThanOs36 <lads42279@gmail.com>
2024-02-18 22:40:31 +0530

    testing

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -7,7 +7,7 @@ function HeroCard() {
                 <div className="hero__card">
                     <img src="./src/images/cars/car1.jpg" alt="img" />
                     <div className="card__info">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsa.
+                        Lorem ipsum dolor sit amet consectetur adipisicing  elit. Totam, ipsa.
                     </div>
                 </div>
                 <div className="hero__card">
