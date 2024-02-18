6582e50a3dec6589566e53736f02ef38acada935
ImThanOs36 <lads42279@gmail.com>
2024-02-18 22:42:21 +0530

    commit

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-
+import car from '../images/cars/car1.jpg'
 function HeroCard() {
     return (
         <div className="container">
             <div className="hero__cards">
                 <div className="hero__card">
-                    <img src="./src/images/cars/car1.jpg" alt="img" />
+                    <img src={`${car}`} alt="img" />
                     <div className="card__info">
                         Lorem ipsum dolor sit amet consectetur adipisicing  elit. Totam, ipsa.
                     </div>
                 </div>
                 <div className="hero__card">
-                    <img src="./src/images/cars/car1.jpg" alt="img" />
+                    <img src={`${car}`} alt="img" />
                     <div className="card__info">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsa.
                     </div>
                 </div>
                 <div className="hero__card">
-   
-                    <img src="./src/images/cars/car1.jpg" alt="img" />
+
+                    <img src={`${car}`} alt="img" />
                     <div className="card__info">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsa.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ipsa.
